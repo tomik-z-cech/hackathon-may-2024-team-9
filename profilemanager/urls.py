efc4c2396e5191ad2d1f51cae43da2d7b47f8538
Tomas Kubancik <tkubancik@gmail.com>
2024-05-24 00:38:38 +0100

    Add about,howto+play templates+links

--- profilemanager/urls.py
+++ profilemanager/urls.py
@@ -5,4 +5,7 @@ from profilemanager import views
 urlpatterns = [
     path("profilemanager", login_required(views.ProfileManagerView.as_view()), name="profile_manager"),
     path("topplayers", views.TopPlayersView.as_view(), name="top_players"),
+    path("about-us", views.AboutUsView.as_view(), name="about_us"),
+    path("how-to-play", views.HowToPlayView.as_view(), name="how_to_play"),
+    path("play", login_required(views.PlayView.as_view()), name="play"),
 ]
